22d49747289d83d1b9d25f1fda8637325ca2713f
Renan Dias <112900871+WHrez1ns@users.noreply.github.com>
2023-05-26 18:23:52 -0300

    Primeira versão da nova feature

--- App.py
+++ App.py
@@ -3,8 +3,10 @@ from tkinter import ttk, messagebox
 import psutil
 import wmi
 from Process import Process
+import hashlib
 
 
+# Checkbox checked
 def check_one_checked():
     if checkbox1_state.get() == 1:
         checkbox2.deselect()
@@ -15,30 +17,107 @@ def check_two_checked():
         checkbox1.deselect()
 
 
+# Mapeando processos
 def mapping_processes():
     for processo in processos:
         try:
             new_process = Process(
                 processo.name(), processo.pid, processo.status())
+            processos_lista.append(new_process)
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
 
 
-def analyzing_process():
-    print("Chamando as técnicas de análise")
-
-
+# Alertas
 def show_help():
     messagebox.showinfo("Manual", "Modos de execução:\n\n - Modo de verificação: este modo é responsável por analisar o próximo processo que for executado na máquina e determinar se ele é um possível malware.\n - Modo constante: este modo é responsável por verificar todos os processos que forem executados na máquina e determinar se eles podem ser possíveis malwares.\n\nComo usar:\n\n1. Selecione a caixa correspondente ao modo de execução desejado.\n2. Clique em 'Iniciar execução'.")
 
 
-# def show_alert():
-#     resposta = messagebox.askyesno(
-#         "Pergunta", "Você deseja fazer backup dos dados? (Recomendado)")
-#     if resposta:
-#         print("Iniciando backup...")
-#     else:
-#         messagebox.showinfo("Alerta", "Certo, você optou por não realizar o backup dos dados neste momento. Lembre-se de que essa opção pode ser selecionada posteriormente, caso você queira fazer o backup dos seus dados para garantir sua segurança. Recomendamos que você faça o backup regularmente para evitar a perda de informações importantes. Se surgir a necessidade, você pode acessar a opção de backup nas configurações do Aegis Defender. Fique à vontade para escolher essa opção sempre que considerar apropriado")
+def on_new_process_created(process):
+    print(f"\033[36m| Novo processo encontrado: {process.Name}")
+
+
+# Técnicas de analise
+def verificar_hash_processo(process, lista_hashes_conhecidos):
+    try:
+        processid = psutil.Process(process.ProcessId)
+        estado_processo = processid.as_dict(attrs=['memory_info'])
+
+        # Calcular o hash do estado do processo
+        hash_objeto = hashlib.sha256(str(estado_processo).encode())
+        hash_calculado = hash_objeto.hexdigest()
+
+        # Comparar o hash calculado com a lista de hashes conhecidos
+        if hash_calculado in lista_hashes_conhecidos:
+            # status id 2
+            print(
+                f"\033[36m| O hash do processo {process.Name} é conhecido. [PERIGOSO]")
+        else:
+            # status id 0
+            print(
+                f"\033[36m| O hash do processo {process.Name} não é conhecido.[SEGURO]")
+    except psutil.NoSuchProcess:
+        # status id 1
+        print(f"\033[36m| Processo {process.Name} não encontrado.")
+    except psutil.AccessDenied:
+        # status id 1
+        print(
+            f'f"\033[36m| Permissão negada para encerrar o processo {process.Name}.')
+    except IndexError:
+        # status id 1
+        print(f"\033[36m| Processo {process.Name} não encontrado.")
+    except wmi.x_wmi:
+        # status id 1
+        print(
+            f'f"\033[36m| Permissão negada para encerrar o processo {process.Name}.')
+
+
+def verificar_instancia_processo(process):
+    try:
+        processid = psutil.Process(process.ProcessId)
+
+        if int(process.ReadOperationCount) > 50:
+            if int(process.WriteOperationCount) > 10:
+                if process.PageFaults > 1000:
+                    if process.ThreadCount > 5:
+                        if process.HandleCount > 200:
+                            processid.terminate()
+                            # status id 2
+                            print(f"\033[36m| [PERIGOSO] {process.Name}")
+                        else:
+                            # status id 1
+                            print(f"\033[36m| [SUSPEITO] {process.Name}")
+                    else:
+                        # status id 1
+                        print(f"\033[36m| [SUSPEITO] {process.Name}")
+                else:
+                    # status id 1
+                    print(f"\033[36m| [SUSPEITO] {process.Name}")
+            else:
+                print(f"\033[36m| [SUSPEITO] {process.Name}")  # status id 1
+        elif process.CommandLine == '"C:\Program Files\Malware\malware.exe" --payload':
+            processid.terminate()
+            print(f"\033[36m| {process.Name} [PERIGOSO]")  # status id 2
+        elif process.KernelModeTime == "00:03:25":
+            print(f"\033[36m| [SUSPEITO] {process.Name}")  # status id 1
+        else:
+            print(f"\033[36m| [SEGURO] {process.Name}")  # status id 0
+    except psutil.NoSuchProcess:
+        # status id 1
+        print(f"\033[36m| [SUSPEITO] Processo {process.Name} não encontrado.")
+    except psutil.AccessDenied:
+        # status id 1
+        print(
+            f'f"\033[36m| [SUSPEITO] Permissão negada para encerrar o processo {process.Name}.')
+    except IndexError:
+        # status id 1
+        print(f"\033[36m| [SUSPEITO] Processo {process.Name} não encontrado.")
+    except wmi.x_wmi:
+        # status id 1
+        print(
+            f'f"\033[36m| [SUSPEITO] Permissão negada para encerrar o processo {process.Name}.')
+
+# Modos de execução
 
 
 def verify_mode():
@@ -49,6 +128,9 @@ def verify_mode():
         while checkbox1_state.get() == 1:
             process = watcher()
             on_new_process_created(process)
+            # iniciar analise
+            verificar_hash_processo(process, lista_hashs)
+            verificar_instancia_processo(process)
             checkbox1.deselect()
             print("\033[31m| Modo de verificação: DESLIGADO")
     except Exception as e:
@@ -57,14 +139,10 @@ def verify_mode():
 
 def constant_mode():
     print("\033[32mModo constante: LIGADO")
-    print("\033[31mModo de verificação: DESLIGADO")
+    print("\033[31mModo constante: DESLIGADO")
 
 
-def on_new_process_created(process):
-    messagebox.showwarning(
-        "Aviso", f"Novo processo encontrado: {process.Name}")
-
-
+# Tratando as opções do usuário
 def start_exe():
     if checkbox1_state.get() == checkbox2_state.get():
         messagebox.showwarning(
@@ -77,10 +155,10 @@ def start_exe():
         constant_mode()
 
 
+lista_hashs = ['hash1', 'hash2', 'hash3']
 processos_lista = []
 processos = psutil.process_iter()
 mapping_processes()
-# show_alert()
 
 root = tk.Tk()
 icon = "img/aegis.ico"
@@ -90,7 +168,6 @@ root.geometry("400x200")
 menubar = tk.Menu(root)
 menu_principal = tk.Menu(menubar, tearoff=0)
 menu_principal.add_command(label="Ajuda", command=show_help)
-# menu_principal.add_command(label="Backup", command=show_alert)
 menu_principal.add_command(label="Sair", command=exit)
 menubar.add_cascade(label="Configurações", menu=menu_principal)
 root.config(menu=menubar)
