d554a1b9ecf9b0b23886e1033bf09be40740b6d7
Renan Dias <112900871+WHrez1ns@users.noreply.github.com>
2023-09-19 19:23:04 -0300

    Aegis Defender - 4.1.2
    - Alterando a linguagem da interface gráfica para inglês;
    - Adicionando um Check no Dashboard para o usuário conferir a quantidade de processos analisados;
    - Criando duas funções e javascript na hora de consultar o arquivo .json.

--- Flask/static/js/dashboard.js
+++ Flask/static/js/dashboard.js
@@ -8,11 +8,22 @@ checkbtn.addEventListener("click", () => {
     checkbtn.classList.toggle("selected");
     if ( checkbtn.classList.contains("selected") ) { 
         detection_in_real_time = true;
-        dashboard_header_title.innerHTML = "Estamos protegendo você <i class='bx bx-shield-quarter'></i>";
-        dashboard_checks.children.item(0).innerHTML = "<div class='dashboard-check-button checked'></div> Modo de detecção em tempo real ativado";
+        dashboard_header_title.innerHTML = "We are protecting you <i class='bx bx-shield-quarter'></i>";
+        dashboard_checks.children.item(0).innerHTML = "<div class='dashboard-check-button checked'></div> Real-time detection mode activated";
     } else {
         detection_in_real_time = false;
-        dashboard_header_title.innerHTML = "Ative o modo de execução <i class='bx bxs-error'></i>";
-        dashboard_checks.children.item(0).innerHTML = "<div class='dashboard-check-button unchecked'></div> Modo de detecção em tempo real desativado";
+        dashboard_header_title.innerHTML = "Activate run mode <i class='bx bxs-error'></i>";
+        dashboard_checks.children.item(0).innerHTML = "<div class='dashboard-check-button unchecked'></div> Real-time detection mode disabled";
     }
 })
+
+function processes_analyzed() {
+    fetch('http://127.0.0.1:5000/static/json/process.json')
+        .then(response => response.json())
+        .then(jsonContent => {
+            let processesAnalyzed = jsonContent["length"];
+            dashboard_checks.children.item(3).innerHTML = `<div class='dashboard-check-button checked'></div> Processes analyzed: ${processesAnalyzed}`;
+        })
+}
+
+processes_analyzed()
--- Flask/static/js/process.js
+++ Flask/static/js/process.js
@@ -1,34 +1,37 @@
 const processTable = document.querySelector(".processes table");
 
-fetch('http://127.0.0.1:5000/static/json/process.json')
-  .then(response => response.json())
-  .then(jsonContent => {
-    jsonContent.forEach(element => {
-        let newProcess = document.createElement('tr');
-        newProcess.className = 'process';
-        let processName = document.createElement('td');
-        processName.textContent = `${element["index"]}`;
-        let processSid = document.createElement('td');
-        processSid.textContent = `${element["name"]}`;
-        let processIndex = document.createElement('td');
-        processIndex.textContent = `${element["sid"]}`
-        let processStatus = document.createElement('td');
-        if (element["sid"] === 0) {
-            processStatus.textContent = "SAFE";
-            processStatus.className = "safe"
-        } else if (element["sid"] === 1) {
-            processStatus.textContent = "SUSPECT";
-            processStatus.className = "suspect"
-        } else if (element["sid"] === 2) {
-            processStatus.textContent = "THREAT";
-            processStatus.className = "threat"
-        } else {
-            processStatus.textContent = "NOT MAPPED";
-        }
-        newProcess.appendChild(processName);
-        newProcess.appendChild(processSid);
-        newProcess.appendChild(processIndex);
-        newProcess.appendChild(processStatus);
-        processTable.appendChild(newProcess);
-    });
-  })
+function list_and_show_processes_analyzed() {
+    fetch('http://127.0.0.1:5000/static/json/process.json')
+        .then(response => response.json())
+        .then(jsonContent => {
+            processesAnalyzed = jsonContent["length"]
+            jsonContent.forEach(element => {
+                let newProcess = document.createElement('tr');
+                newProcess.className = 'process';
+                let processName = document.createElement('td');
+                processName.textContent = `${element["index"]}`;
+                let processSid = document.createElement('td');
+                processSid.textContent = `${element["name"]}`;
+                let processIndex = document.createElement('td');
+                processIndex.textContent = `${element["sid"]}`
+                let processStatus = document.createElement('td');
+                if (element["sid"] === 0) {
+                    processStatus.textContent = "SAFE";
+                    processStatus.className = "safe"
+                } else if (element["sid"] === 1) {
+                    processStatus.textContent = "SUSPECT";
+                    processStatus.className = "suspect"
+                } else if (element["sid"] === 2) {
+                    processStatus.textContent = "THREAT";
+                    processStatus.className = "threat"
+                } else {
+                    processStatus.textContent = "NOT MAPPED";
+                }
+                newProcess.appendChild(processName);
+                newProcess.appendChild(processSid);
+                newProcess.appendChild(processIndex);
+                newProcess.appendChild(processStatus);
+                processTable.appendChild(newProcess);
+            });
+        })
+}
